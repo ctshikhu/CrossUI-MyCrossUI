fb4234c13f0c62da48a4917cc388ea01071c418f
ctshikhu <35162373+ctshikhu@users.noreply.github.com>
2021-08-08 15:47:21 +0200

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,48 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.FormLayout")
+                .setHost(host,"xui_ui_formlayout1")
+                .setLeft("2.2857142857142856em")
+                .setTop("3.0476190476190474em")
+                .setWidth("56.68571428571428em")
+                .setHeight("24.99047619047619em")
+                .setLayoutData({
+                    "rows" : 5,
+                    "cols" : 5,
+                    "rowSetting" : {
+                        "1" : {
+                            "manualHeight" : 50
+                        },
+                        "2" : {
+                            "manualHeight" : 50
+                        },
+                        "3" : {
+                            "manualHeight" : 50
+                        },
+                        "4" : {
+                            "manualHeight" : 50
+                        },
+                        "5" : {
+                            "manualHeight" : 50
+                        }
+                    }
+                })
+            );
+            
+            host.xui_ui_formlayout1.append(
+                xui.create("xui.UI.Image")
+                .setHost(host,"xui_ui_image5")
+                .setLeft("-0.0761904761904762em")
+                .setTop("-0.8380952380952381em")
+                .setWidth("11.2em")
+                .setHeight("5.942857142857143em")
+                .setSrc("{xui.ini.img_pic}"),
+                "A1"
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
